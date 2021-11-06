9df34f8afc78445e24e44310c83b6fc9ea66f182
Mayada Al-Sawah <mayada.alsawah@ekreative.com>
2021-11-06 22:19:50 +0200

    Add LanguageUtils

--- mvc/src/main/java/com/mayada1994/mydictionary/fragments/AddLanguagesFragment.kt
+++ mvc/src/main/java/com/mayada1994/mydictionary/fragments/AddLanguagesFragment.kt
@@ -1,11 +1,10 @@
 package com.mayada1994.mydictionary.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mayada1994.mydictionary.R
+import androidx.fragment.app.Fragment
 import com.mayada1994.mydictionary.databinding.FragmentAddLanguagesBinding
 
 class AddLanguagesFragment : Fragment() {
