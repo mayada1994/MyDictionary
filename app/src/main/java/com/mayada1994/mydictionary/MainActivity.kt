1be0c377f627c22f7951d8033fa225e333be05ac
Mayada Al-Sawah <mayada.alsawah@ekreative.com>
2021-11-06 21:06:48 +0200

    Add icons and splash screen

--- app/src/main/java/com/mayada1994/mydictionary/MainActivity.kt
+++ app/src/main/java/com/mayada1994/mydictionary/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mayada1994.mydictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.mayada1994.mydictionary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
