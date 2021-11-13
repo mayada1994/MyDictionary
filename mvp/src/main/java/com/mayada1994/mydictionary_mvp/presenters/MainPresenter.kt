9ff5c21c36c83502d8a5624a61a5c08e81bfd37c
Mayada Al-Sawah <mayada.alsawah@ekreative.com>
2021-11-13 23:07:38 +0200

    Add AddLanguagesFragment

--- mvp/src/main/java/com/mayada1994/mydictionary_mvp/presenters/MainPresenter.kt
+++ mvp/src/main/java/com/mayada1994/mydictionary_mvp/presenters/MainPresenter.kt
@@ -1,17 +1,19 @@
 package com.mayada1994.mydictionary_mvp.presenters
 
 import com.mayada1994.mydictionary_mvp.contracts.MainContract
+import com.mayada1994.mydictionary_mvp.di.DictionaryComponent
+import com.mayada1994.mydictionary_mvp.fragments.AddLanguagesFragment
 
 class MainPresenter(
     private val viewInterface: MainContract.ViewInterface
 ) : MainContract.PresenterInterface {
 
     override fun init() {
-//        if (DictionaryComponent.cacheUtils.defaultLanguage.isNullOrBlank()) {
-//            viewInterface.setFragment(AddLanguagesFragment::class.java)
-//        } else {
+        if (DictionaryComponent.cacheUtils.defaultLanguage.isNullOrBlank()) {
+            viewInterface.setFragment(AddLanguagesFragment::class.java)
+        } else {
 //            viewInterface.setFragment(MainFragment::class.java)
-//        }
+        }
     }
 
 }
